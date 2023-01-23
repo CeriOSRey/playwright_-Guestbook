5ddb45738062cae5285b657ef449542c8f55ba36
CeriOSRey <rey.cerio@outlook.com>
2023-01-23 02:23:36 -0800

    Add locators and actions for sign the gb flow

---
+++ actions/SignTheGuestbookPage.action.ts
@@ -0,0 +1,57 @@
+import { Page } from "@playwright/test"
+import * as userData from "../data/userdata.json"
+import SignTheGuestbookLocator from "../locators/signTheGuestBookPageLocator"
+
+export default class SignTheGuestbookActions {
+
+    public page: Page
+    public locator: SignTheGuestbookLocator
+
+    constructor(page: Page) {
+        this.locator = new SignTheGuestbookLocator(page)
+        this.page = page
+    }
+
+    async fillNameField(name: string) {
+        await this.locator.nameField.fill(name)
+    }
+
+    async fillEmailField(email:string) {
+        await this.locator.emailField.fill(email)
+    }
+
+    async fillCommentArea(comment:string) {
+        await this.locator.emailField.fill(comment)
+    }
+
+    async checkSubscribeBox() {
+        await this.locator.subscribeCheckBox.click()
+    }
+
+    async checkFemaleRadialButton() {
+        await this.locator.femaleRadialButton.click()
+    }
+
+    async checkmaleRadialButton() {
+        await this.locator.maleRadialButton.click()
+    }
+
+    async pickPlaywrightAutomationTool() {
+        await this.locator.testToolDropDown.click()
+        await this.locator.playwrightOption.click()
+    }
+
+    async clickSubmitButton() {
+        await this.locator.submitButton.click()
+    }
+    
+    async fillWholeForm() {
+        await this.fillNameField(userData.signTheGuestBookFormData.name)
+        await this.fillEmailField(userData.signTheGuestBookFormData.email)
+        await this.fillCommentArea(userData.signTheGuestBookFormData.comment)
+        await this.checkSubscribeBox()
+        await this.checkFemaleRadialButton()
+        await this.pickPlaywrightAutomationTool()
+        await this.clickSubmitButton()
+    }
+}
--- common/urls.ts
+++ common/urls.ts
@@ -1,4 +1,5 @@
 export const urls = {
     baseUrl: 'https://testautomationpro.com/aut/',
     homeUrl: 'https://testautomationpro.com/aut/index.php',
+    loginUrl: 'https://testautomationpro.com/aut/login.php',
 }
